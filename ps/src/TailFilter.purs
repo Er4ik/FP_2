ed2bc8794817a5d3f02e4b6796a1528b47af4754
Per4ik <ervin.osmanov.007@gmail.com>
2022-10-20 00:26:54 +0300

    Update TailFilter.purs

--- ps/src/TailFilter.purs
+++ ps/src/TailFilter.purs
@@ -1,19 +1,11 @@
 module TailFilter where
 
 import Prelude
-
 import Effect (Effect)
 import Effect.Console (log)
-
 import Data.List (List(..), length, reverse, (:))
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-
--- tail recursion filter
--- Оптимізуйте реалізацію фільтра використовуючи підхід хвостової оптимізації
--- tailFilter :: forall a. (a -> Boolean) -> List a -> List a -> List a
--- tailFilter _ Nil acc = acc
--- tailFilter f (x : xs) acc = if f x then tailFilter f xs (x : acc) else tailFilter f xs acc
 
 tailFilter :: forall a. (a -> Boolean) -> List a -> List a -> List a
 tailFilter p (x : xs) ys = 
